d3881c83e8c5a9f0f1888e127df2a42e0850a556
olesia <infocooll.col@gmail.com>
2022-08-10 20:27:20 +0200

    Update the time

--- src/app.js
+++ src/app.js
@@ -1,3 +1,27 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
+
 function displayTemperature(response) {
   console.log(response.data);
   let temperatureElement = document.querySelector("#temperature");
@@ -12,6 +36,8 @@ function displayTemperature(response) {
   feelsLike.innerHTML = Math.round(response.data.main.feels_like);
   let windElement = document.querySelector("#wind");
   windElement.innerHTML = Math.round(response.data.wind.speed);
+  let dataElemet = document.querySelector("#date");
+  dataElemet.innerHTML = formatDate(response.data.dt * 1000);
 }
 let apiKey = "32326959e29561c07003e2cd9a21f791";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Kyiv&appid=${apiKey}&units=metric`;
