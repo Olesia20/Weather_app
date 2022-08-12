dd65ffd16f25b7cd5cefafdf52a6d5960935215a
olesia <infocooll.col@gmail.com>
2022-08-12 18:57:01 +0200

    Added forecast api

--- src/app.js
+++ src/app.js
@@ -45,7 +45,11 @@ function displauForecast() {
 
   forecastHTML = forecastHTML + `</div>`;
   forcastElement.innerHTML = forecastHTML;
-  console.log(forecastHTML);
+}
+function getForecast(coordinates) {
+  let apiKey = "32326959e29561c07003e2cd9a21f791";
+  let apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  console.log(apiUrl);
 }
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
@@ -69,7 +73,7 @@ function displayTemperature(response) {
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
-  getForcaste(response.data.coord);
+  getForecast(response.data.coord);
 }
 
 function search(city) {
@@ -82,7 +86,6 @@ function handleSubmit(event) {
   event.preventDefault();
   let cityInputElement = document.querySelector("#city-input");
   search(cityInputElement.value);
-  console.log(cityInputElement.value);
 }
 function displayFuhrenheitTemperature(event) {
   event.preventDefault();
